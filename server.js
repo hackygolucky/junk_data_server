b1fe8ba18854f0a9b627a3cfbbca6f6d503d94d6
Tracy Abrahms <tracyabrahms@gmail.com>
2013-03-28 23:06:22 -0700

    clear out garbled stream and server mess

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // Junk data server
-var junkdata_stream = require('/junkdata-stream')
-	, through = require('through')
- 
-var server = junkdata(
-  33  // milliseconds between junk!
-, {   // shape of junk!
-    keyname: Number
-  , otherKey: String
-  , someOtherKey: Boolean
-  }
-)
- 
-junkdata_stream.on('data', console.log)
+var junkdata_stream = require('./junkdata-stream')
+	, http = require('http')
+	,	fs = require('fs')
 
+var server = http.createServer(function (req, res) {
+		var ttl = 33
+			, mappings = {
+			    keyname: Number
+			  , otherKey: String
+			  , someOtherKey: Boolean
+		  }
+    var stream = junkdata_stream(ttl, mappings);
+    stream.on('error', function (err) {
+        res.statusCode = 500;
+        res.end(String(err));
+    });
+    stream.pipe(res);
+});
 
-
-
-
-
-
+server.listen(8000)
